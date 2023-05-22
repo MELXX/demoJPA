1396007a1460b74752bac41d724826f0b50931a0
kmgwenya <kmgwenya@retrorabbit.co.za>
2023-05-22 14:14:10 +0200

    added flyway migration config

--- src/main/java/com/example/demoJPA/Configuration/PostgresDataSourceConfig.java
+++ src/main/java/com/example/demoJPA/Configuration/PostgresDataSourceConfig.java
@@ -1,6 +1,8 @@
 package com.example.demoJPA.Configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,12 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class PostgresDataSourceConfig {
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.jdbc-url}")
+    private String url;
     @Bean
     @Primary
     @ConfigurationProperties("spring.datasource")
@@ -18,4 +26,15 @@ public class PostgresDataSourceConfig {
                 .type(HikariDataSource.class)
                 .build();
     }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public Flyway flyway(){
+        Flyway flyway = Flyway.configure().dataSource(url, username, password).load();
+        flyway.migrate();
+        return  flyway;
+    }
+
+
 }
