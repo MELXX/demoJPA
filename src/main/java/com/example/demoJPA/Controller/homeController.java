3e47fda73091e55041ee8abf3cf226dada5cdab5
kmgwenya <kmgwenya@retrorabbit.co.za>
2023-08-12 15:00:52 +0200

    access changes

--- src/main/java/com/example/demoJPA/Controller/homeController.java
+++ src/main/java/com/example/demoJPA/Controller/homeController.java
@@ -1,6 +1,5 @@
 package com.example.demoJPA.Controller;
 
-import com.example.demoJPA.Repositories.UserRepository;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,24 +14,22 @@ import java.io.IOException;
 @RestController("/api/")
 public class homeController {
 
-    @Autowired
-    UserRepository userRepository;
 
     @GetMapping("/hello")
     public String Test(){
         return "Hello";
     }
 
-    @PostMapping("/Login")
-    public String Login(String password,String email){
-        ;
-        return userRepository.Login(email,password)?"Can log in":"nahh bruv";
-    }
+
 
     @SneakyThrows
     @GetMapping("/testAccess")
     public String Access() throws IOException {
-        Database db = DatabaseBuilder.open(new File("mydb.mdb"));
+        Table db = DatabaseBuilder.open(new File("C:\\Users\\melusi\\Downloads\\RecycleSA.accdb"))
+                .getTable("tblMaterials");
+        for(Row row : db) {
+            System.out.println("Column 'a' has value: " + row.get("Type"));
+        }
         return "";
     }
 
--- src/main/java/com/example/demoJPA/Models/User.java
+++ src/main/java/com/example/demoJPA/Models/User.java
@@ -1,13 +1,11 @@
 package com.example.demoJPA.Models;
 
-import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class User {
-    @jakarta.persistence.Id
     private UUID Id;
     private String Name;
     private String Email;
