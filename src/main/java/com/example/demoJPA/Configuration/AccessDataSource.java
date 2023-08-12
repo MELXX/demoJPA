1364c03709371a8644457342ef12ea97de182cad
kmgwenya <kmgwenya@retrorabbit.co.za>
2023-08-12 14:33:31 +0200

    access config

---
+++ src/main/java/com/example/demoJPA/Configuration/AccessDataSource.java
@@ -0,0 +1,11 @@
+package com.example.demoJPA.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccessDataSource {
+
+
+
+}
--- src/main/java/com/example/demoJPA/Controller/homeController.java
+++ src/main/java/com/example/demoJPA/Controller/homeController.java
@@ -1,10 +1,16 @@
 package com.example.demoJPA.Controller;
 
 import com.example.demoJPA.Repositories.UserRepository;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.healthmarketscience.jackcess.*;
+
+import java.io.File;
+import java.io.IOException;
+
 
 @RestController("/api/")
 public class homeController {
@@ -21,6 +27,13 @@ public class homeController {
     public String Login(String password,String email){
         ;
         return userRepository.Login(email,password)?"Can log in":"nahh bruv";
+    }
+
+    @SneakyThrows
+    @GetMapping("/testAccess")
+    public String Access() throws IOException {
+        Database db = DatabaseBuilder.open(new File("mydb.mdb"));
+        return "";
     }
 
 }
