928e9dae5479775528ddfbc714b368fd4ef7fc2b
kmgwenya <kmgwenya@retrorabbit.co.za>
2023-08-13 17:36:42 +0200

    moved to static folder

---
+++ src/main/resources/static/js/tokenvalidator.js
@@ -0,0 +1,27 @@
+// const serverpath = 'http://localhost:8080'
+// let z =  localStorage.getItem('RecycleSAsessionToken')
+// if(z == null){
+//     window.location.href = "login.html"
+// }else{
+//     const url = `${serverpath}/api/validate`;
+//     fetch(url, {
+//         method: "POST",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//           },
+//         body:JSON.stringify({"token":z})
+//     }).then((response)=>{
+//         if (!response.ok) {
+//             alert("session expired please login again")
+//             window.location.href = "login.html"
+//         }
+//     });
+
+   
+// }
+
+// function logout(){
+//     localStorage.clear();
+//     window.location.href = "login.html"
+// }
